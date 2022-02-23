c7735df1366bb3c796a9a508f9fe6faf5bfdd98c
simonhassler-stat <simon.hassler@statworx.com>
2022-02-23 09:39:09 +0100

    Second commit

---
+++ R/main.R
@@ -0,0 +1,267 @@
+#' statworx Theme based on theme_classic
+#' @description Sets a pre-defined theme as the standard ggplot theme via
+#' \code{\link[ggplot2:theme_get]{theme_set}} and \code{\link[ggplot2:ggtheme]{theme_classic}}
+#' @keywords theme
+#' @inheritParams ggplot2::theme_classic
+#' @export
+#' @examples
+#' ggplot(iris, aes(Sepal.Width, Sepal.Length, color = Species)) +   geom_point() +  statworx_theme_1()
+#'
+statworx_theme_1 <- function(){
+
+  font <- "Arial"   #assign font family up front
+
+  theme_set(theme_classic())
+
+  theme_replace(
+
+    #grid elements
+    panel.grid.major = element_blank(),
+    panel.grid.minor = element_blank(),
+    axis.ticks.x = element_line(colour = "#283440"),
+    axis.ticks.y = element_line(colour = "#283440"),
+
+
+
+    #text elements
+    plot.title = element_text(
+      family = font,
+      colour = "#0000FF",
+      size = 20,
+      face = 'bold',
+      hjust = 0,
+      vjust = 2),
+
+    plot.subtitle = element_text(
+      family = font,
+      colour = "#000000",
+      size = 14,
+      hjust = 0,
+      vjust = 2),
+
+    plot.caption = element_text(
+      family = font,
+      colour = "#283440",
+      size = 9,
+      hjust = 1),
+
+    axis.title = element_text(
+      family = font,
+      colour = "#000000",
+      size = 11),
+
+    axis.text = element_text(
+      colour = "#283440",
+      family = font,
+      size = 11),
+
+    axis.text.x = element_text(
+      margin=margin(5, b = 10))
+
+    #since the legend often requires manual tweaking
+    #based on plot content, don't define it here
+  )
+
+}
+
+#' statworx Theme based on theme_minimal
+#' @description Sets a pre-defined theme as the standard ggplot theme via
+#' \code{\link[ggplot2:theme_get]{theme_set}} and \code{\link[ggplot2:ggtheme]{theme_minimal}}
+#' @keywords theme
+#' @inheritParams ggplot2::theme_minimal
+#' @export
+#' @examples
+#' ggplot(iris, aes(Sepal.Width, Sepal.Length, color = Species)) +   geom_point() +  statworx_theme_2()
+
+statworx_theme_2 <- function(){
+
+  font <- "Arial"   #assign font family up front
+
+  theme_set(theme_minimal())
+
+  theme_replace(
+
+    #grid elements
+    panel.grid.minor.x = element_blank(),
+    panel.grid.minor.y = element_blank(),
+    #axis.line = element_line(color = statworx_cols("grey_1")),
+    axis.ticks.x = element_line(colour = "#283440"),
+    axis.ticks.y = element_line(colour = "#283440"),
+
+
+
+    #text elements
+    plot.title = element_text(
+      family = font,
+      colour = "#0000FF",
+      size = 20,
+      face = 'bold',
+      hjust = 0,
+      vjust = 2),
+
+    plot.subtitle = element_text(
+      family = font,
+      colour = "#000000",
+      size = 14,
+      hjust = 0,
+      vjust = 2),
+
+    plot.caption = element_text(
+      family = font,
+      colour = "#283440",
+      size = 9,
+      hjust = 1),
+
+    axis.title = element_text(
+      family = font,
+      colour = "#000000",
+      size = 11),
+
+    axis.text = element_text(
+      colour = "#283440",
+      family = font,
+      size = 11),
+
+    axis.text.x = element_text(
+      margin=margin(5, b = 10))
+
+    #since the legend often requires manual tweaking
+    #based on plot content, don't define it here
+  )
+
+}
+
+
+# Define statworx colors
+statworx_colors <- c(
+  "Tech Blue" = "#0000FF",
+  "Deep Black" = "#000000",
+  "Super White" = "#FFFFFF",
+  "grey_1" = "#283440",
+  "grey_2" = "#6C7D8C",
+  "grey_3" = "#B6BDCC",
+  "grey_4" = "#EBF0F2",
+  "accent_1" = "#FFFF00",
+  "accent_2" = "#FE0D6C",
+  "accent_3" = "#00C800"
+)
+
+# Extended list of statworx colors
+statworx_colors_blue_black <- c(
+  "Tech Blue" = "#0000FF",
+  "blue_1" = "#3343FF",
+  "blue_2" = "#6573FF",
+  "blue_3" = "#98A3FF",
+  "white_1" = "#CCD1FF",
+  "white_2" = "#F2F3FF",
+  "Deep Black" = "#000000",
+  "dark_1" = "#333333",
+  "dark_2" = "#666666",
+  "grey_1" = "#999999",
+  "grey_2" = "#CCCCCC"
+)
+
+
+#' Wrap color vector in function.
+#'
+#' @param colors List of colors (either statworx_colors or statworx_colors_blue_black)
+#' @param ... Name of color from "statworx_colors"
+#' @return Hex code of the color
+create_color_vector <- function(colors = statworx_colors, ...) {
+  cols <- c(...)
+
+  if (is.null(cols))
+    return (colors)
+
+  colors[cols]
+}
+
+
+#' @export
+# Make list of different palettes
+statworx_palettes <- list(
+  "statworx_palette" = create_color_vector(colors = statworx_colors),
+  "statworx_palette_without_white" = create_color_vector(colors = statworx_colors,-c(3)),
+  "statworx_palette_accent" = create_color_vector(colors = statworx_colors, c(1, 2, 9, 10, 8)),
+  "statworx_palette_good_to_bad" = create_color_vector(colors = statworx_colors, c(9, 5, 4, 2, 1)),
+  "statworx_palette_blue" = create_color_vector(colors = statworx_colors_blue_black, c(1, 2, 3, 4, 5, 6)),
+  "statworx_palette_black" = create_color_vector(colors = statworx_colors_blue_black, c(7, 8, 9, 10, 11, 6))
+)
+
+
+#' Create a color palette function.
+#'
+#' @param palette A color palette or "custom".
+#' @param reverse If true, order of palette is reversed.
+#' @param col_list Provide a vector of colors if @param palette = "custom".
+#' @return Palette generating function based on "colorRampPalette".
+#'
+create_statworx_palette <- function(palette = "statworx_palette",
+                                    reverse = FALSE, col_list = "", ...) {
+  if (palette == "custom" & is.null(col_list)) {
+    palette = "statworx_palette"
+  }
+
+  if (palette == "custom") {
+    pal <- statworx_colors[col_list]
+  } else {
+    pal <- statworx_palettes[[palette]]
+  }
+
+  if (reverse)
+    pal <- rev(pal)
+
+  colorRampPalette(pal, ...)
+}
+
+
+#' Create a constructor for ggplot for lines, points, etc.
+#'
+#' @param ... arguments passed to \code{\link[ggplot2]{discrete_scale}}
+#' @param palette A color palette.
+#' @param discrete If true, discrete palette is returned. Otherwise, continuous palette.
+#' @param reverse If true, order of palette is reversed.
+#' @param col_list Provide a vector of colors if parameter palette="custom".
+#' @return Scale constructor for ggplot.
+#' @keywords scale_color_statworx()
+#' @export
+#' @examples
+#' ggplot(iris, aes(Sepal.Width, Sepal.Length, color = Species)) +   geom_point() + scale_color_statworx()
+#'
+scale_color_statworx <- function(palette = "statworx_palette", discrete = TRUE,
+                                 reverse = FALSE, col_list = c(), ...) {
+
+  pal <- create_statworx_palette(palette = palette, reverse = reverse, col_list = col_list)
+
+  if (discrete) {
+    discrete_scale("colour", paste0("statworx_", palette), palette = pal, ...)
+  } else {
+    scale_color_gradientn(colours = pal(256), ...)
+  }
+}
+
+
+#' Create a constructor for ggplot for box plots, bar plots, etc.
+#'
+#' @param ... arguments passed to \code{\link[ggplot2]{discrete_scale}}
+#' @param palette A color palette.
+#' @param discrete If true, discrete palette is returned. Otherwise, continuous palette.
+#' @param reverse If true, order of palette is reversed.
+#' @param col_list Provide a vector of colors if parameter palette="custom".
+#' @return Scale constructor for ggplot.
+#' @keywords scale_fill_statworx()
+#' @export
+#' @examples
+#' ggplot(iris, aes(Sepal.Width, Sepal.Length, color = Species)) +   geom_point() +  scale_fill_statworx()
+#'
+scale_fill_statworx <- function(palette = "statworx_palette", discrete = TRUE,
+                                reverse = FALSE, col_list = c(), ...) {
+
+  pal <- create_statworx_palette(palette = palette, reverse = reverse, col_list = col_list)
+
+  if (discrete) {
+    discrete_scale("fill", paste0("statworx_", palette), palette = pal, ...)
+  } else {
+    scale_fill_gradientn(colours = pal(256), ...)
+  }
+}
